0f55a3bcc5816fb79bb870d55821d077a783ccaa
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-01-23 12:43:51 -0300

    feature: add header

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,9 +1,15 @@
 import React, { createContext, useCallback, useContext, useState } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: Object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -12,7 +18,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: Object;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
