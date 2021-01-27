bcafa62141405ba32323fb8191245da52cd5beac
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-01-27 20:24:38 -0300

    feature: add appointments list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,6 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { isToday, format, parseISO, isAfter } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
@@ -24,6 +26,17 @@ interface MonthAvailabilityItem {
   day: number;
   available: boolean;
 }
+
+interface IAppointment {
+  id: string;
+  date: string;
+  hourFormatted: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
 
@@ -33,8 +46,10 @@ const Dashboard: React.FC = () => {
     MonthAvailabilityItem[]
   >([]);
 
+  const [appointments, setAppointments] = useState<IAppointment[]>([]);
+
   const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
-    if (modifiers.available) {
+    if (modifiers.available && !modifiers.disabled) {
       setSelectedDate(day);
     }
   }, []);
@@ -56,6 +71,24 @@ const Dashboard: React.FC = () => {
       });
   }, [currentMonth, user.id]);
 
+  useEffect(() => {
+    api
+      .get<IAppointment[]>('/appointments/me', {
+        params: {
+          year: selectedDate.getFullYear(),
+          month: selectedDate.getMonth() + 1,
+          day: selectedDate.getDate(),
+        },
+      })
+      .then(response => {
+        const appointmentsFormatted = response.data.map(appointment => ({
+          ...appointment,
+          hourFormatted: format(parseISO(appointment.date), 'HH:mm'),
+        }));
+        setAppointments(appointmentsFormatted);
+      });
+  }, [selectedDate]);
+
   const disabledDays = useMemo(() => {
     const dates = monthAvailability
       .filter(monthDay => monthDay.available === false)
@@ -68,6 +101,32 @@ const Dashboard: React.FC = () => {
 
     return dates;
   }, [currentMonth, monthAvailability]);
+
+  const selectedDateAsText = useMemo(() => {
+    return format(selectedDate, "'Dia' dd 'de' MMMM", { locale: ptBR });
+  }, [selectedDate]);
+
+  const selectedWeekDay = useMemo(() => {
+    return format(selectedDate, 'cccc', { locale: ptBR });
+  }, [selectedDate]);
+
+  const morningAppointments = useMemo(() => {
+    return appointments.filter(appointment => {
+      return parseISO(appointment.date).getHours() < 12;
+    });
+  }, [appointments]);
+
+  const afternoonAppointments = useMemo(() => {
+    return appointments.filter(appointment => {
+      return parseISO(appointment.date).getHours() >= 12;
+    });
+  }, [appointments]);
+
+  const nextAppointment = useMemo(() => {
+    return appointments.find(appointment =>
+      isAfter(parseISO(appointment.date), new Date()),
+    );
+  }, [appointments]);
 
   return (
     <Container>
@@ -91,37 +150,74 @@ const Dashboard: React.FC = () => {
         <Schedule>
           <h1>Horários agendados</h1>
           <p>
-            <span>Hoje</span>
-            <span>Dia 6</span>
-            <span>Segunda-feira</span>
+            {isToday(selectedDate) && <span>Hoje</span>}
+            <span>{selectedDateAsText}</span>
+            <span>{selectedWeekDay}</span>
           </p>
-          <NextAppointment>
-            <strong>Atendimento a seguir</strong>
-            <div>
-              <img src="" alt="" />
-              <strong>GAbriel Oshiro</strong>
-              <span>
-                <FiClock />
-                Appointment
-              </span>
-            </div>
-          </NextAppointment>
+
+          {isToday(selectedDate) && nextAppointment && (
+            <NextAppointment>
+              <strong>Agendamento a seguir</strong>
+              <div>
+                <img
+                  src={nextAppointment.user.avatar_url}
+                  alt={nextAppointment.user.name}
+                />
+                <strong>{nextAppointment.user.name}</strong>
+                <span>
+                  <FiClock />
+                  {nextAppointment.hourFormatted}
+                </span>
+              </div>
+            </NextAppointment>
+          )}
 
           <Section>
             <strong>Manhã</strong>
-            <Appointment>
-              <span>
-                <FiClock />
-                08:00
-              </span>
-              <div>
-                <img src="" alt="" />
-              </div>
-            </Appointment>
+
+            {morningAppointments.length === 0 && (
+              <p>Nenhum agendamento nesse período.</p>
+            )}
+
+            {morningAppointments.map(appointment => (
+              <Appointment key={appointment.id}>
+                <span>
+                  <FiClock />
+                  {appointment.hourFormatted}
+                </span>
+                <div>
+                  <img
+                    src={appointment.user.avatar_url}
+                    alt={appointment.user.name}
+                  />
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </Appointment>
+            ))}
           </Section>
 
           <Section>
             <strong>Tarde</strong>
+
+            {afternoonAppointments.length === 0 && (
+              <p>Nenhum agendamento nesse período.</p>
+            )}
+
+            {afternoonAppointments.map(appointment => (
+              <Appointment key={appointment.id}>
+                <span>
+                  <FiClock />
+                  {appointment.hourFormatted}
+                </span>
+                <div>
+                  <img
+                    src={appointment.user.avatar_url}
+                    alt={appointment.user.name}
+                  />
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </Appointment>
+            ))}
           </Section>
         </Schedule>
         <Calendar>
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -13,16 +13,13 @@ export const HeaderContent = styled.div`
   margin: 0 auto;
   display: flex;
   align-items: center;
-
   > img {
     height: 80px;
   }
-
   button {
     margin-left: auto;
     background: transparent;
     border: 0;
-
     svg {
       color: #999591;
       width: 20px;
@@ -35,25 +32,25 @@ export const Profile = styled.div`
   display: flex;
   align-items: center;
   margin-left: 80px;
-
   img {
     width: 56px;
     height: 56px;
     border-radius: 50%;
   }
-
   div {
     display: flex;
     flex-direction: column;
     margin-left: 16px;
     line-height: 24px;
-
     span {
       color: #f4ede8;
     }
-
-    strong {
+    a {
+      text-decoration: none;
       color: #ff9000;
+      &:hover {
+        opacity: 0.8;
+      }
     }
   }
 `;
@@ -67,23 +64,19 @@ export const Content = styled.main`
 export const Schedule = styled.div`
   flex: 1;
   margin-right: 120px;
-
   h1 {
     font-size: 36px;
   }
-
   p {
     margin-top: 8px;
     color: #ff9000;
     display: flex;
     align-items: center;
     font-weight: 500;
-
     span {
       display: flex;
       align-items: center;
     }
-
     span + span::before {
       content: '';
       width: 1px;
@@ -96,13 +89,11 @@ export const Schedule = styled.div`
 
 export const NextAppointment = styled.div`
   margin-top: 64px;
-
   > strong {
     color: #999591;
     font-size: 20px;
-    font-weight: 4000;
+    font-weight: 400;
   }
-
   div {
     background: #3e3b47;
     display: flex;
@@ -111,34 +102,30 @@ export const NextAppointment = styled.div`
     border-radius: 10px;
     margin-top: 24px;
     position: relative;
-
     &::before {
+      content: '';
       position: absolute;
       height: 80%;
       width: 1px;
       left: 0;
       top: 10%;
-      content: '';
       background: #ff9000;
     }
-
     img {
       width: 80px;
       height: 80px;
       border-radius: 50%;
+      background: #ff9000;
     }
-
     strong {
       margin-left: 24px;
       color: #fff;
     }
-
     span {
       margin-left: auto;
       display: flex;
       align-items: center;
       color: #999591;
-
       svg {
         color: #ff9000;
         margin-right: 8px;
@@ -149,7 +136,6 @@ export const NextAppointment = styled.div`
 
 export const Section = styled.section`
   margin-top: 48px;
-
   > strong {
     color: #999591;
     font-size: 20px;
@@ -159,123 +145,89 @@ export const Section = styled.section`
     padding-bottom: 16px;
     margin-bottom: 16px;
   }
+  > p {
+    color: #999591;
+  }
 `;
 
 export const Appointment = styled.div`
   display: flex;
   align-items: center;
-
   & + div {
-    margin-top: 16;
+    margin-top: 16px;
   }
-
   span {
     margin-left: auto;
     display: flex;
     align-items: center;
     color: #f4ede8;
-
+    width: 70px;
     svg {
       color: #ff9000;
       margin-right: 8px;
     }
   }
-
   div {
     flex: 1;
     background: #3e3b47;
     display: flex;
     align-items: center;
-    padding: 16px 24px;
+    padding: 16px;
     border-radius: 10px;
     margin-left: 24px;
-
     img {
       width: 56px;
       height: 56px;
       border-radius: 50%;
+      background: #ff9000;
     }
-
     strong {
-      margin-left: 24px;
-      color: #fff;
+      margin-left: 16px;
+      color: #f4ede8;
       font-size: 20px;
+      font-weight: 500;
     }
   }
 `;
 
 export const Calendar = styled.aside`
   width: 380px;
-
   .DayPicker {
+    background: #28262e;
     border-radius: 10px;
   }
-
   .DayPicker-wrapper {
     padding-bottom: 0;
-    background: #3e3b47;
-    border-radius: 10px;
   }
-
   .DayPicker,
   .DayPicker-Month {
     width: 100%;
   }
-
-  .DayPicker-NavButton {
-    color: #999591 !important;
-  }
-
-  .DayPicker-NavButton--prev {
-    right: auto;
-    left: 1.5em;
-    margin-right: 0;
-  }
-
   .DayPicker-Month {
     border-collapse: separate;
     border-spacing: 8px;
-    margin: 16px 0 0 0;
-    padding: 16px;
-    background-color: #28262e;
-    border-radius: 0 0 10px 10px;
+    margin: 16px;
   }
-
-  .DayPicker-Caption {
-    margin-bottom: 1em;
-    padding: 0 1em;
-    color: #f4ede8;
-
-    > div {
-      text-align: center;
-    }
-  }
-
   .DayPicker-Day {
     width: 40px;
     height: 40px;
   }
-
   .DayPicker-Day--available:not(.DayPicker-Day--outside) {
     background: #3e3b47;
     border-radius: 10px;
     color: #fff;
   }
-
   .DayPicker:not(.DayPicker--interactionDisabled)
     .DayPicker-Day:not(.DayPicker-Day--disabled):not(.DayPicker-Day--selected):not(.DayPicker-Day--outside):hover {
     background: ${shade(0.2, '#3e3b47')};
   }
-
   .DayPicker-Day--today {
     font-weight: normal;
   }
-
   .DayPicker-Day--disabled {
     color: #666360 !important;
     background: transparent !important;
   }
-
   .DayPicker-Day--selected {
     background: #ff9000 !important;
     border-radius: 10px;
