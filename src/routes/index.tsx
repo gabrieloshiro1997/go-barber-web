07512f8d3cc499cfb839a1c41672bf28f5083542
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-01-27 21:49:16 -0300

    feature: add update profile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,6 +9,7 @@ import ForgotPassword from '../pages/ForgotPassword';
 
 import Dashboard from '../pages/Dashboard';
 import ResetPassword from '../pages/ResetPassword';
+import Profile from '../pages/Profile';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -18,6 +19,7 @@ const Routes: React.FC = () => (
     <Route path="/reset-password" exact component={ResetPassword} />
 
     <Route path="/dashboard" exact component={Dashboard} isPrivate />
+    <Route path="/profile" exact component={Profile} isPrivate />
   </Switch>
 );
 
